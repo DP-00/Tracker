74c817c2b907229d02514a50d7c83d04e581448b
dpasiak <57316056+DP-00@users.noreply.github.com>
2026-04-08 01:05:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.getElementById("daily-stats-limits").textContent = `📵 ${Math.floor((
    DROPBOX
 ========================= */
 
-// const REDIRECT_URI = `${window.location.origin}/Tracker/`; //"http://localhost:8000/";
-const REDIRECT_URI = "http://localhost:8000/"; //"http://localhost:8000/";
+const REDIRECT_URI = `${window.location.origin}/Tracker/`; //"http://localhost:8000/";
+// const REDIRECT_URI = "http://localhost:8000/"; //"http://localhost:8000/";
 const CLIENT_ID = "7ctgzhwolmiq6kc"; // <-- your client id
 let dbxAuth = new Dropbox.DropboxAuth({ clientId: CLIENT_ID });
 dbx = new Dropbox.Dropbox({ auth: dbxAuth });
