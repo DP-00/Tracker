3facfa25ce47b518330a60b896bc44881605b6da
dpasiak <57316056+DP-00@users.noreply.github.com>
2026-04-12 14:53:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let dayOfWeek = new Date().toLocaleString("default", {
    DROPBOX
 ========================= */
 
-// const REDIRECT_URI = `${window.location.origin}/Tracker/`; //"http://localhost:8000/";
-const REDIRECT_URI = "http://localhost:8000/"; //"http://localhost:8000/";
+const REDIRECT_URI = `${window.location.origin}/Tracker/`; //"http://localhost:8000/";
+// const REDIRECT_URI = "http://localhost:8000/"; //"http://localhost:8000/";
 const CLIENT_ID = "7ctgzhwolmiq6kc"; // <-- your client id
 let dbxAuth = new Dropbox.DropboxAuth({ clientId: CLIENT_ID });
 dbx = new Dropbox.Dropbox({ auth: dbxAuth });
